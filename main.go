18d7d3807e2c2608ffb401efc9cb11083fc573c2
Jason Poon <git@jasonpoon.ca>
2017-11-22 22:53:59 -0500

    regenerate map on every poll

--- main.go
+++ main.go
@@ -19,27 +19,23 @@ const (
 )
 
 var (
-	bootnodes = make(map[string]string)
-	enodes    string
+	enodes string
 )
 
 func getEnodes(addressRecord string) {
 	ipAddresses, err := ResolveAddressRecord(addressRecord)
 	if err != nil {
-		log.Errorf("Error resolving A record: %s", err)
+		log.Errorf("Error resolving DNS address record: %s", err)
 		return
 	}
 
-	log.Printf("A Record {%s} resolved to  %s", addressRecord, ipAddresses)
-	for _, ipAddress := range ipAddresses {
-		if _, ok := bootnodes[ipAddress]; ok {
-			log.Printf("%s. Already exists.", ipAddresses)
-			continue
-		}
+	log.Printf("%s resolved to %s", addressRecord, ipAddresses)
 
+	var bootnodes = make(map[string]struct{})
+	for _, ipAddress := range ipAddresses {
 		resp, err := http.Get(fmt.Sprintf("http://%s:%s", ipAddress, "8080"))
 		if err != nil {
-			log.Errorf("Error on HTTP GET: %s", err)
+			log.Errorf("Error retrieving enode address: %s", err)
 			return
 		}
 		defer resp.Body.Close()
@@ -49,14 +45,15 @@ func getEnodes(addressRecord string) {
 			return
 		}
 
-		bootnodes[ipAddress] = strings.TrimSpace(string(contents))
-		log.Infof("%s. Adding %s", ipAddress, bootnodes[ipAddress])
+		var enodeAddress = strings.TrimSpace(string(contents))
+		bootnodes[enodeAddress] = struct{}{}
+		log.Infof("%s with enode address %s", ipAddress, enodeAddress)
 	}
 
 	i := 0
 	buffer := bytes.NewBufferString("")
-	for _, v := range bootnodes {
-		buffer.WriteString(v)
+	for k := range bootnodes {
+		buffer.WriteString(k)
 
 		if i < len(bootnodes)-1 {
 			buffer.WriteString(",")
@@ -75,7 +72,7 @@ func startPollGetEnodes(addressRecord string) {
 }
 
 func webHandler(w http.ResponseWriter, r *http.Request) {
-	log.Infof("Request from %s", r.RemoteAddr)
+	log.Infof("handling request from %s", r.RemoteAddr)
 	fmt.Fprintln(w, enodes)
 }
 
@@ -88,7 +85,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	log.Infof("Starting bootnode-registrar. {%s}.", *bootNodeService)
+	log.Infof("starting bootnode-registrar. {%s}.", *bootNodeService)
 
 	go startPollGetEnodes(*bootNodeService)
 	http.HandleFunc("/", webHandler)
